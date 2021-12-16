78403de0569a1f6a729f82cc2bf8f256fdade6ca
Robert Kleemann <robert@kleemann.org>
2021-12-16 15:06:43 -0800

    give the gaming layer a green underlight

--- keyboards/kbdfans/kbd75/keymaps/kleemann/stuff.c
+++ keyboards/kbdfans/kbd75/keymaps/kleemann/stuff.c
@@ -1,14 +1,17 @@
 #include QMK_KEYBOARD_H
 
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case 4: // gaming layer
     case 5: // gaming toggle layeer
-        rgblight_disable_noeeprom();
+        rgblight_sethsv_noeeprom(85, 0xff, 0x40); // h=85=green, s=0xff=sharp color, v=0x40 dim
         break;
     default: //  for any other layers, or the default layer
-        rgblight_enable_noeeprom();
-        rgblight_sethsv_noeeprom(0, 0xff, 0x40); // h=0=red, s=0xff=sharp color, v=0x10 dim
+        rgblight_sethsv_noeeprom(0, 0xff, 0x40); // h=0=red, s=0xff=sharp color, v=0x40 dim
         break;
     }
     return state;
