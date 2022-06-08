4652a7fa68f0420393c0b1dadce4118620943591
Robert Kleemann <robert@kleemann.org>
2022-06-08 16:41:55 -0700

    playing with backlight enable

---
+++ keyboards/dz60/keymaps/kleemann/rules.mk
@@ -0,0 +1,9 @@
+
+# the case doesn't allow backlight to come through so keep these
+# off. Strangely I need this to be enabled so that I can turn the
+# light off. This is done with the keyboard via F-Q once after
+# flashing. If I wanted to avoid this I could add some startup code
+# that disables it.
+
+#BACKLIGHT_ENABLE = no
+#RGBLIGHT_ENABLE = no
