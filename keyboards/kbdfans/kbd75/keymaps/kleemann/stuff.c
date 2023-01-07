22cb8a78ecab52f14df614b5c0ee911d26ca6ef2
Robert Kleemann <robert@kleemann.org>
2023-01-07 15:18:43 -0800

    normal is the default layer; made reset a bit harder to hit; fixed robert layout to match other keyboards

--- keyboards/kbdfans/kbd75/keymaps/kleemann/stuff.c
+++ keyboards/kbdfans/kbd75/keymaps/kleemann/stuff.c
@@ -2,12 +2,13 @@
 
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(85, 0xff, 0x40); // h=85=green, s=0xff=sharp color, v=0x40 dim
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-    case 4: // gaming layer
-    case 5: // gaming toggle layeer
+    case 0: // gaming/normal layer
+    case 5: // gaming/normal toggle layeer
         rgblight_sethsv_noeeprom(85, 0xff, 0x40); // h=85=green, s=0xff=sharp color, v=0x40 dim
         break;
     default: //  for any other layers, or the default layer
