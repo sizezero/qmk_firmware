9c4b5d099227354213cdb4e517f5a5d008ac26a4
Robert Kleemann <robert@kleemann.org>
2024-06-24 06:37:34 -0700

    get rid of matrix pins since we are using convert

--- keyboards/reviung/reviung41/keymaps/kleemann/config.h
+++ keyboards/reviung/reviung41/keymaps/kleemann/config.h
@@ -1,4 +1,10 @@
-#define MATRIX_ROWS 7
-#define MATRIX_COLS 6
-#define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP22, GP20, GP23 }
-#define MATRIX_COL_PINS { GP4, GP5, GP6, GP7, GP8, GP9 }
+
+// QMK discord says that I don't need to change the matrix pins.
+// The remapping feature will fix things.
+// One guy said it might be cleaner to just branch a new keyboard revision, enter
+// new pins, and fix it that way.
+
+//#define MATRIX_ROWS 7
+//#define MATRIX_COLS 6
+//#define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP22, GP20, GP23 }
+//#define MATRIX_COL_PINS { GP4, GP5, GP6, GP7, GP8, GP9 }
